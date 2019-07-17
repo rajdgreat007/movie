d5866553bef6167ae997d7229ff0947a965661d1
Raj Gupta <raj.gupta@walmartlabs.com>
2019-07-18 00:32:07 +0530

    added util function to return data with mocked images

---
+++ src/mock/images.js
@@ -0,0 +1,18 @@
+const mockImagesFn = () => {
+  let mockImages = [];
+  let index = 1;
+  for (let i = 1; i <= 20; i++) {
+    index = index > 9 ? 1 : index;
+    mockImages.push({
+      images: {
+        original: {
+          url: `/mock-images/giphy${index++}.gif`
+        }
+      },
+      id: new Date().getUTCMilliseconds() * Math.random()
+    });
+  }
+  return mockImages;
+};
+
+export default mockImagesFn;
