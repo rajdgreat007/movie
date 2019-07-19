f4164a7001b546ce87028a69993872789b95af4b
Raj Gupta <raj.gupta@walmartlabs.com>
2019-07-19 16:05:02 +0530

    improved user interaction with search box - submitting search term using enter key

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,22 +4,28 @@ import "./SearchBar.css";
 class SearchBar extends React.Component {
   state = { searchTerm: "" };
 
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSearchSubmit(this.state.searchTerm);
+  };
+
   render() {
     return (
       <div className="SearchBar">
-        <input
-          type="text"
-          className="SearchInput"
-          value={this.state.searchTerm}
-          onChange={e => this.setState({ searchTerm: e.target.value })}
-          placeholder="Search for an awesome gif"
-        />
-        <button
-          className="SearchButton"
-          onClick={() => this.props.onSearchSubmit(this.state.searchTerm)}
-        >
-          <i className="material-icons">search</i>
-        </button>
+        <form onSubmit={this.onFormSubmit}>
+          <input
+            type="text"
+            className="SearchInput"
+            value={this.state.searchTerm}
+            onChange={e => this.setState({ searchTerm: e.target.value })}
+            onClick={e => this.setState({ searchTerm: "" })}
+            placeholder="Search for awesome gifs"
+            autoFocus
+          />
+          <button type="submit" className="SearchButton">
+            <i className="material-icons">search</i>
+          </button>
+        </form>
       </div>
     );
   }
