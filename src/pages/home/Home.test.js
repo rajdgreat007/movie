9bf76fbc2ffcb4df7eacf4185834cd62db0fe753
Raj Gupta <raj.gupta@walmartlabs.com>
2019-07-19 21:26:49 +0530

    added tests for home component

---
+++ src/pages/home/Home.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Home from "./Home";
+
+describe("Home Component", () => {
+  const HomeComponent = shallow(<Home />);
+
+  it("renders home component", () => {
+    expect(HomeComponent.hasClass("Home")).toEqual(true);
+  });
+
+  it("renders header", () => {
+    expect(HomeComponent.find("header")).toHaveLength(1);
+  });
+
+  it("renders logo", () => {
+    expect(HomeComponent.find("img").hasClass("Home-logo")).toEqual(true);
+  });
+
+  it("renders search bar", () => {
+    expect(HomeComponent.find("SearchBar")).toHaveLength(1);
+  });
+
+  it("renders a Redirect if redirect key is set to true in state", () => {
+    HomeComponent.setState({ redirect: true });
+    expect(HomeComponent.find("Redirect")).toHaveLength(1);
+  });
+});
